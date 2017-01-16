f865ee52eec2a55e3c6b8f776b39a6315e2fee60
Bogdan <skrynnyk.b@gmail.com>
2017-01-16 17:26:36 +0200

    Update UsrClientUnit.js

--- UsrClientUnit.js
+++ UsrClientUnit.js
@@ -1,5 +1,6 @@
 ////////////////////////////
-//JS Client Module 
+//JS Client Page Module
+//////////////////////////
 //////////////////////////
 define("UsrClientUnit", ['ext-base', 'terrasoft', 'ServiceHelper'], function(Ext, Terasoft, ServiceHelper) {
 	var getView = function() {
